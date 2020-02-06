181ad19087c6f8c38c877b5114bdf7054332ecf7
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-06 21:42:50 +0000

    create ref

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    // create reference to get details from element in DOM
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.imageRef);
+  }
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
