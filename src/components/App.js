dc01e564319dc5443e1b623851e976b74cf4573f
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-06 14:34:35 +0000

    get back data from api and display some of it

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,22 @@ import SearchBar from "./SearchBar";
 require("dotenv").config();
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] }; // expect array so initialize with empty array
+
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY
       }
     });
-  }
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
