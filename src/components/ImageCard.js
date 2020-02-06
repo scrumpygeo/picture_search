4351f2e246532b86f5c67a452506d0e5f70c0780
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-06 22:07:14 +0000

    tweak image widths with refs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,18 +3,29 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { spans: 0 };
+
     // create reference to get details from element in DOM
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageRef);
+    this.imageRef.current.addEventListener("load", this.setSpans);
   }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    // [clientHeight  / grid-auto-rows height (10px)]
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans: spans });
+  };
 
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
