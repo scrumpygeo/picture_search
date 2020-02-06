acd4b0cbe926db0d9136265328a4527c2be76942
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-06 13:46:38 +0000

    create .env, add unsplash keys to it and add .env to gitignore; add axios ref with constant and test = ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+require("dotenv").config();
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY
+      }
+    });
   }
   render() {
     return (
